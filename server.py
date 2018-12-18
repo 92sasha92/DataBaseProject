ea20a9d141316716965567b19ff15bf7bafeff68
sasha <92sasha92@gmail.com>
2018-12-18 15:00:42 +0200

    starting to fix the routing

--- server.py
+++ server.py
@@ -13,5 +13,15 @@ def home_page():
     return render_template('pages/main_page.html')
 
 
+@app.route('/ethnic_cuisines_start')
+def ethnic_cuisines_page():
+    return render_template('pages/ethnic_cuisines_start.html')
+
+
+@app.route('/ethnic_cuisines')
+def ethnic_cuisines():
+    return render_template('pages/ethnic_cuisines.html')
+
+
 if __name__ == "__main__":
     app.run()
