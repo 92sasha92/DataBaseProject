f62ab8b2e7b4bf467189f29f822949ed49595958
sasha <92sasha92@gmail.com>
2018-12-30 10:05:56 +0200

    cocktail db

--- mysql_queries.py
+++ mysql_queries.py
@@ -29,3 +29,36 @@ def insert_recipe_to_db(recipe_id, recipe_name, recipe_img, recipe_ins, prep_tim
         print("Error")
         conn.rollback()
 
+
+def insert_drink_to_db(drink_id, drink_name, drink_category, drink_image, instructions, glass, conn):
+    x = conn.cursor()
+    x.execute("""INSERT INTO Drink VALUES (%d, %s, %s, %s, %s, %s, %s)""",
+              (drink_id, drink_name, drink_category, drink_image, instructions, glass))
+    try:
+        conn.commit()
+    except:
+        print("Error")
+        conn.rollback()
+
+
+def insert_drink_ingredient_to_db(drink_ingredient, drink_measure, conn):
+    x = conn.cursor()
+    x.execute("""INSERT INTO DrinkIngredients VALUES (%s, %s)""",
+              (drink_ingredient, drink_measure))
+    try:
+        conn.commit()
+    except:
+        print("Error")
+        conn.rollback()
+
+
+def insert_to_drink_ingredient_list_db(drink_id, drink_ingredient, drink_measure, conn):
+    x = conn.cursor()
+    x.execute("""INSERT INTO ListOfDrinkIngredients VALUES (%d, %s, %s)""",
+              (drink_id, drink_ingredient, drink_measure))
+    try:
+        conn.commit()
+    except:
+        print("Error")
+        conn.rollback()
+
