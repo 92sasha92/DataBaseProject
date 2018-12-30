7d28e5b17f40f612dea59f4c54d16b2aecf7bacf
sasha <92sasha92@gmail.com>
2018-12-30 10:33:10 +0200

    cocktail db: add get before insert

--- mysql_queries.py
+++ mysql_queries.py
@@ -35,7 +35,10 @@ def insert_recipe_to_db(recipe_id, recipe_name, recipe_img, recipe_ins, prep_tim
 def insert_drink_to_db(drink_id, drink_name, drink_category, drink_image, instructions, glass, conn):
     x = conn.cursor()
     sql = "INSERT INTO Drink VALUES (%d, %s, %s, %s, %s, %s, %s)"
-    x.execute(sql, (drink_id, drink_name, drink_category, drink_image, instructions, glass))
+    sql_get = "SELECT drink_id FROM Drink Where drink_id={}".format(drink_id)
+    is_exit = x.execute(sql_get)
+    if is_exit == 0:
+        x.execute(sql, (drink_id, drink_name, drink_category, drink_image, instructions, glass))
     try:
         conn.commit()
     except:
@@ -46,7 +49,12 @@ def insert_drink_to_db(drink_id, drink_name, drink_category, drink_image, instru
 def insert_drink_ingredient_to_db(drink_ingredient, drink_measure, conn):
     x = conn.cursor()
     sql = "INSERT INTO DrinkIngredients VALUES (%s, %s)"
-    x.execute(sql, (drink_ingredient, drink_measure))
+    sql_get = "SELECT drink_ingredient, drink_measure " \
+              "FROM DrinkIngredients " \
+              "Where drink_ingredient={} AND drink_measure={}".format(drink_ingredient, drink_measure)
+    is_exit = x.execute(sql_get)
+    if is_exit == 0:
+        x.execute(sql, (drink_ingredient, drink_measure))
     try:
         conn.commit()
     except:
@@ -57,7 +65,13 @@ def insert_drink_ingredient_to_db(drink_ingredient, drink_measure, conn):
 def insert_to_drink_ingredient_list_db(drink_id, drink_ingredient, drink_measure, conn):
     x = conn.cursor()
     sql = "INSERT INTO ListOfDrinkIngredients VALUES (%d, %s, %s)"
-    x.execute(sql, (drink_id, drink_ingredient, drink_measure))
+    sql_get = "SELECT drink_id, drink_ingredient, drink_measure " \
+              "FROM ListOfDrinkIngredients " \
+              "Where drink_id={} AND drink_ingredient={} " \
+              "AND drink_measure={}".format(drink_id, drink_ingredient, drink_measure)
+    is_exit = x.execute(sql_get)
+    if is_exit == 0:
+        x.execute(sql, (drink_id, drink_ingredient, drink_measure))
     try:
         conn.commit()
     except:
