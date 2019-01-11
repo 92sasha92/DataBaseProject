ecc0079892225307951f614137f9b875cab03e07
OR <OR@PC>
2019-01-11 09:07:24 +0200

    fix bugs in romantic

--- sql_romantic_queries.py
+++ sql_romantic_queries.py
@@ -9,6 +9,12 @@ seafood_keywords = ['seafood', 'shrimp', 'crab', 'lobster', 'scallop', 'calamari
 
 fish_keywords = ['tuna', 'salmon', 'sardines', 'fish', 'trout', 'bass', 'halibut', 'cod', 'anchovies']
 
+fruit_keywords = ['apple', 'peach', 'strawberries', 'berries', 'grape', 'apricot', 'banana',
+                  'strawberry', 'berry', 'cherries', 'plum', 'fruit', 'fig', 'guava', 'melon',
+                  'lime', 'lemon', 'orange', 'papaya', 'paw paw', 'pineapple']
+
+salad_keywords = ['salad', 'slaw', 'chopped']
+
 
 def get_romantic_meal_results_by_params(main_ingredient, side_ingredient, dessert_taste, max_prep_time):
     with sshtunnel.SSHTunnelForwarder(
@@ -55,12 +61,20 @@ def get_recipe_from_db_by_romantic_meal_filter(main_ingredient, side_ingredient,
                 "(SELECT Recipe.recipe_id, prep_time FROM Recipe, ListOfCourses " \
                 "WHERE Recipe.recipe_id = ListOfCourses.recipe_id AND course_name = 'Desserts') AS desserts " \
                 "WHERE (sides.prep_time + mains.prep_time + desserts.prep_time) <= " + max_prep_time_in_sec + \
-                " AND (" + get_query_of_main_recipes_like_keywords(main_ingredient) + ") AND sides.recipe_id IN " \
-                "(SELECT recipe_id FROM ListOfIngredients WHERE LOWER(ingredient_name) LIKE '%" + side_ingredient + \
-                "%') "
+                " AND (" + get_query_of_recipes_like_keywords(main_ingredient) + ") AND "
+        if side_ingredient.lower() == 'salad':
+            query += ("(" + get_query_of_recipes_like_keywords(side_ingredient) + ") ")
+        else:
+            query += ("sides.recipe_id IN (SELECT recipe_id FROM ListOfIngredients "
+                      "WHERE LOWER(ingredient_name) LIKE '%" + side_ingredient + "%') ")
+
         if dessert_ingredient.lower() == "nosugar":
             query += "AND desserts.recipe_id NOT IN (SELECT recipe_id FROM ListOfIngredients " \
                      "WHERE LOWER(ingredient_name) LIKE '%sugar%')"
+        elif dessert_ingredient.lower() == "fruit":
+            print("fruitttttt")
+            query += ("AND (" + get_query_of_recipes_like_keywords(dessert_ingredient) + ") ")
+            print(query)
         else:
             query += "AND desserts.recipe_id IN (SELECT recipe_id FROM ListOfIngredients WHERE LOWER(ingredient_name) " \
                 "LIKE '%" + dessert_ingredient + "%')"
@@ -81,7 +95,8 @@ def get_recipe_from_db_by_romantic_meal_filter(main_ingredient, side_ingredient,
     return x.fetchall()
 
 
-def get_query_of_main_recipes_like_keywords(main_ingredient):
+def get_query_of_recipes_like_keywords(main_ingredient):
+
     res = ""
     base_query = "mains.recipe_id LIKE '%"
     keywords = []
@@ -91,6 +106,12 @@ def get_query_of_main_recipes_like_keywords(main_ingredient):
         keywords = seafood_keywords
     elif main_ingredient.lower() == 'fish':
         keywords = fish_keywords
+    if main_ingredient.lower() == 'salad':
+        base_query = "sides.recipe_id LIKE '%"
+        keywords = salad_keywords
+    elif main_ingredient.lower() == 'fruit':
+        base_query = "desserts.recipe_id LIKE '%"
+        keywords = fruit_keywords
 
     idx = 0
     for keyword in keywords:
@@ -98,6 +119,7 @@ def get_query_of_main_recipes_like_keywords(main_ingredient):
             res += "OR "
         res += base_query + keyword + "%' "
         idx += 1
+
     return res
 
 
